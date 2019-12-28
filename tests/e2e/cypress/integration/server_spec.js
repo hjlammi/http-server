8fa857fad7ab866eeb17839ff54f89996c61f55c
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-12-28 15:07:45 +0200

    Add e2e test for receiving 400 Not Found response

--- tests/e2e/cypress/integration/server_spec.js
+++ tests/e2e/cypress/integration/server_spec.js
@@ -1,5 +1,21 @@
-describe('My First Test', function() {
-    it('Does not do much!', function() {
+describe('server', function() {
+    beforeEach(function() {
         cy.visit('http://127.0.0.1:8000');
+    });
+
+    it('receives 200 OK response from the server', function() {
+        cy.request('/').as('response');
+        cy.get('@response').should((response) => {
+            expect(response.statusText).to.equal('OK');
+            expect(response.status).to.equal(200);
+        });
+    });
+
+    it('receives 404 Not Found response from the server', function() {
+        cy.request('/').as('response');
+        cy.get('@response').should((response) => {
+            expect(response.statusText).to.equal('Not found');
+            expect(response.status).to.equal(400);
+        });
     });
 });
