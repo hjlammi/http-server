cb90989ff367d84e4a0e6c68f43addee85aa1ea1
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2020-01-12 12:00:04 +0200

    Remove unneeded attribute and related code from Connection

--- server/connection.py
+++ server/connection.py
@@ -9,16 +9,12 @@ class Connection:
         self.data = b''
         self.send_buffer = b''
         self.state = Connection.RECEIVING_REQUEST
-        self.response_left = None
 
         self.socket.setblocking(False)
 
     def send(self, response):
         self.send_buffer += response
         self.state = Connection.SENDING_RESPONSE
-        len_bytes_sent = self.socket.send(response)
-        self.response_left = response[len_bytes_sent:]
-        return len_bytes_sent
 
     def close(self):
         self.socket.close()
