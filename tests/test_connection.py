8490eb1e62c8f7922bba2f73f0592eb1ebf14d7f
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2020-01-12 11:33:36 +0200

    Remove unneeded tests after change to codRemove unneeded tests after change to code

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -29,17 +29,3 @@ def test_cannot_send_after_connection_is_closed():
     connection.close()
     with pytest.raises(Exception, match='Connection closed'):
         connection.send(b'test')
-
-def test_connection_writes_part_of_longer_response_in_send_buffer():
-    connection = Connection(ADDR, fake_socket)
-    response = b'This is a longer test response'
-    len_res_sent = connection.send(response)
-
-    assert fake_socket.send_buffer == response[:len_res_sent]
-
-def test_part_that_was_already_sent_gets_removed_from_response():
-    connection = Connection(ADDR, fake_socket)
-    response = b'This is a longer test response'
-    connection.send(response)
-
-    assert connection.response_left == response[FakeSocket.BUFFER_SIZE:]
