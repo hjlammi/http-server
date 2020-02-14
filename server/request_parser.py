e302e11db6810f51289f896909f6f9a04aa23013
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2020-02-14 08:34:04 +0200

    Accept zero or one header in request

--- server/request_parser.py
+++ server/request_parser.py
@@ -1,13 +1,16 @@
 from lark import Lark, Transformer, v_args
 
 grammar = r'''
-    request: startline
+    request: startline headers?
 
     startline: METHOD WS URI WS VERSION CR LF
+    headers: HEADER_KEY":" WS HEADER_VALUE CR LF
 
     METHOD: "GET"
     URI: /\/[a-zA-Z0-9\/.]+/
     VERSION: "HTTP/1.1"
+    HEADER_KEY: /[a-zA-Z0-9]+/
+    HEADER_VALUE: /[a-zA-Z0-9\/]+/
 
     %import common.WS
     %import common.CR
@@ -22,8 +25,13 @@ def parse_request(request):
 
 class TreeToRequest(Transformer):
     @v_args(inline=True)
-    def request(self, tree):
-        return tree
+    def request(self, startline, headers=None):
+        return {
+            "method": startline["method"],
+            "uri": startline["uri"],
+            "http_version": startline["http_version"],
+            "headers": headers
+        }
 
     @v_args(inline=True)
     def startline(self, method, ws1, uri, ws2, version, cr, lf):
@@ -32,3 +40,9 @@ class TreeToRequest(Transformer):
             uri = uri.value,
             http_version = version.value
         )
+
+    @v_args(inline=True)
+    def headers(self, header_key, ws, header_value, cr, lf):
+        return {
+            header_key: header_value
+        }
--- tests/test_request_parser.py
+++ tests/test_request_parser.py
@@ -4,7 +4,6 @@ import pytest
 def test_parse_request_gets_GET_method_from_the_request():
     request_line = 'GET /path/to/example.com HTTP/1.1\r\n'
     result = parse_request(request_line)
-    print(result)
 
     assert result['method'] == 'GET'
 
@@ -19,3 +18,15 @@ def test_parse_request_gets_http_version_from_the_request():
     result = parse_request(request_line)
 
     assert result['http_version'] == 'HTTP/1.1'
+
+def test_parse_request_parses_host_header_from_the_request():
+    request_line = 'GET /path/to/example.com HTTP/1.1\r\nHost: www.w3.org\r\n'
+    result = parse_request(request_line)
+
+    assert result['headers']['Host'] == 'www.w3.org'
+
+def test_parse_request_parses_accept_header_from_the_request():
+    request_line = 'GET /path/to/example.com HTTP/1.1\r\naccept: text/html\r\n'
+    result = parse_request(request_line)
+
+    assert result['headers']['accept'] == 'text/html'
