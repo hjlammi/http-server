9ba2509bdc0b20ec22af1431c894ce8e68fd424d
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2020-02-15 19:56:46 +0200

    Create dict of headers recursively

--- server/request_parser.py
+++ server/request_parser.py
@@ -5,7 +5,7 @@ grammar = r'''
     request: startline headers*
 
     startline: METHOD WS URI WS VERSION CR LF
-    headers: HEADER_KEY":" WS HEADER_VALUE CR LF
+    headers: (headers)* HEADER_KEY":" WS HEADER_VALUE CR LF
 
     METHOD: "GET"
     URI: /\/[a-zA-Z0-9\/.]+/
@@ -34,7 +34,7 @@ class TreeToRequest(Transformer):
             startline["method"],
             startline["uri"],
             startline["http_version"],
-            tuple_to_dict(headers)
+            headers[0] if headers else None
         )
 
     @v_args(inline=True)
@@ -46,10 +46,13 @@ class TreeToRequest(Transformer):
         )
 
     @v_args(inline=True)
-    def headers(self, header_key, ws, header_value, cr, lf):
-        return {
-            header_key.value: header_value.value
-        }
+    def headers(self, *header_args):
+        headers = header_args[0]
+        if (isinstance(headers, dict)):
+            headers.update({header_args[1].value: header_args[3].value})
+            return headers
+        else:
+            return {header_args[0].value: header_args[2].value}
 
 def tuple_to_dict(headers):
     headers_in_dict = {}
