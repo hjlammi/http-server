983073f9c84871a480977abb0dcd82ac752dc4d4
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2020-02-16 09:09:11 +0200

    Rename variables in parser test

--- tests/test_request_parser.py
+++ tests/test_request_parser.py
@@ -2,55 +2,55 @@ from server.request_parser import parse_request
 import pytest
 
 def test_parse_request_gets_GET_method_from_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert result.method == 'GET'
+    assert request.method == 'GET'
 
 def test_parse_request_gets_uri_from_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert result.uri == '/path/to/example.com'
+    assert request.uri == '/path/to/example.com'
 
 def test_parse_request_gets_http_version_from_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert result.http_version == 'HTTP/1.1'
+    assert request.http_version == 'HTTP/1.1'
 
 def test_parse_request_sets_headers_to_None_when_there_are_no_headers_in_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert result.headers == None
+    assert request.headers == None
 
 def test_parse_request_parses_host_header_from_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\nHost: www.w3.org\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\nHost: www.w3.org\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert result.headers['Host'] == 'www.w3.org'
+    assert request.headers['Host'] == 'www.w3.org'
 
 def test_parse_request_parses_accept_header_from_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\naccept: text/html\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\naccept: text/html\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert result.headers['accept'] == 'text/html'
+    assert request.headers['accept'] == 'text/html'
 
 def test_parse_request_parses_two_headers_from_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\nHost: www.w3.org\r\naccept: text/html\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\nHost: www.w3.org\r\naccept: text/html\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert len(result.headers) == 2
+    assert len(request.headers) == 2
 
 def test_parse_request_parses_accept_and_host_headers_from_the_request():
-    request_line = 'GET /path/to/example.com HTTP/1.1\r\nHost: www.w3.org\r\naccept: text/html\r\n\r\n'
-    result = parse_request(request_line)
+    request_string = 'GET /path/to/example.com HTTP/1.1\r\nHost: www.w3.org\r\naccept: text/html\r\n\r\n'
+    request = parse_request(request_string)
 
-    assert result.headers['accept'] == 'text/html'
-    assert result.headers['Host'] == 'www.w3.org'
+    assert request.headers['accept'] == 'text/html'
+    assert request.headers['Host'] == 'www.w3.org'
 
 # # RFC2616 4.1. Message Types
 def test_parse_request_should_not_throw_if_empty_lines_before_startline():
-    request_line = '\r\n\r\nGET /path/to/example.com HTTP/1.1\r\naccept: text/html\r\n\r\n'
-    parse_request(request_line)
+    request_string = '\r\n\r\nGET /path/to/example.com HTTP/1.1\r\naccept: text/html\r\n\r\n'
+    parse_request(request_string)
