5b88bf8065205da9bd2ecb20b075fd411da5d108
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2020-03-01 10:36:25 +0200

    Generate response for GET request

--- server/generate_response.py
+++ server/generate_response.py
@@ -2,11 +2,15 @@ from .response import Response
 
 BODY = '<h1>Hello World</h1>\r\n'
 def generate_response(request):
+    body = None
     if request.uri == '/':
         status_code = 200
         headers = [
             'Content-Type: text/html',
             f'Content-Length: {len(BODY)}'
         ]
-    response = Response(status_code, headers, None)
+
+        if request.method == 'GET':
+            body = BODY
+    response = Response(status_code, headers, body)
     return response.serialize()
--- tests/test_generate_response.py
+++ tests/test_generate_response.py
@@ -7,3 +7,8 @@ def test_generate_response_for_HEAD_request():
     request = Request('HEAD', '/', None, None, None)
     expected_result = b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 22\r\n\r\n'
     assert generate_response(request) == expected_result
+
+def test_generate_response_for_GET_request():
+    request = Request('GET', '/', None, None, None)
+    expected_result = b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 22\r\n\r\n<h1>Hello World</h1>\r\n'
+    assert generate_response(request) == expected_result
