98bfd56051348021caabf517edf46dc47b99d06a
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2020-03-03 12:13:35 +0200

    Get contents of a dir in a sorted list

--- server/generate_response.py
+++ server/generate_response.py
@@ -1,3 +1,4 @@
+from os import scandir
 from .response import Response
 
 BODY_SUCCESS = '<h1>Hello World</h1>\r\n'
@@ -23,3 +24,17 @@ def generate_response(request, path_to_serve):
         body = BODY_NOT_FOUND
     response = Response(status_code, headers, body)
     return response.serialize()
+
+def get_contents_from_dir(path):
+    dirs = []
+    files = []
+    with scandir(path) as scandir_iterator:
+        for entry in scandir_iterator:
+            if entry.is_dir():
+                dirs.append(f'{entry.name}/')
+            if entry.is_file():
+                files.append(entry.name)
+    files.sort()
+    dirs.sort()
+    dirs.extend(files)
+    return dirs
--- tests/test_generate_response.py
+++ tests/test_generate_response.py
@@ -1,5 +1,5 @@
 from server.request import Request
-from server.generate_response import generate_response
+from server.generate_response import generate_response, get_contents_from_dir
 import pytest
 
 def test_generate_response_for_HEAD_request():
@@ -11,3 +11,15 @@ def test_generate_response_for_GET_request():
     request = Request('GET', '/', None, None, None)
     expected_result = b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 22\r\n\r\n<h1>Hello World</h1>\r\n'
     assert generate_response(request, None) == expected_result
+
+@pytest.mark.focus
+def test_get_contents_from_root():
+    result = get_contents_from_dir('tests/webroot')
+
+    assert result == ['cat_pics/', 'random/', 'lorem_ipsum.text']
+
+@pytest.mark.focus
+def test_get_contents_from_subdir():
+    result = get_contents_from_dir('tests/webroot/cat_pics')
+
+    assert result == ['2janu.jpg', 'ella.jpg', 'janu.jpg']
